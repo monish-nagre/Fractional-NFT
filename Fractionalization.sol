2b01e13deca02741595c288b001bacda2c7cb4d0
monish-nagre <monishnagre22@gmail.com>
2024-01-22 15:12:18 +0530

    Update fractional contract

--- Fractionalization.sol
+++ Fractionalization.sol
@@ -7,46 +7,66 @@ import  "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
 import  "@openzeppelin/contracts@4.6.0/token/ERC20/extensions/draft-ERC20Permit.sol";
 import  "@openzeppelin/contracts@4.6.0/token/ERC721/utils/ERC721Holder.sol";
 
-contract  FractionalizedNFT  is  ERC20, Ownable, ERC20Permit, ERC721Holder {
- IERC721  public  collection;
- uint256  public  tokenId;
- bool  public  initialized = false;
- bool  public  forSale = false;
- uint256  public  salePrice;
- bool  public  canRedeem = false;
+contract FractionalizedNFT is ERC20, Ownable, ERC20Permit, ERC721Holder {
+    struct FractionalizedNFTInfo {
+        IERC721 collection;
+        uint256 tokenId;
+        bool initialized;
+        bool forSale;
+        uint256 salePrice;
+        bool canRedeem;
+    }
 
- constructor() ERC20("RealEstate", "RETK") ERC20Permit("RealEstate") {}
+    mapping(uint256 => FractionalizedNFTInfo) public fractionalizedNFTs;
 
- function  initialize(address  _collection, uint256  _tokenId, uint256  _amount) external  onlyOwner {
-  require(!initialized, "Already initialized");
-  require(_amount > 0, "Amount needs to be more than 0");
-  collection = IERC721(_collection);
-  collection.safeTransferFrom(msg.sender, address(this), _tokenId);
-  tokenId = _tokenId;
-  initialized = true;
-  _mint(msg.sender, _amount);
- }
+    constructor() ERC20("EGEMONEY", "EGEM") ERC20Permit("MYEGEM") {}
 
 
- function  putForSale(uint256  price) external  onlyOwner {
-  salePrice = price;
-  forSale = true;
- }
 
- function  purchase() external  payable {
-  require(forSale, "Not for sale");
-  require(msg.value >= salePrice, "Not enough ETH sent");
-  collection.transferFrom(address(this), msg.sender, tokenId);
-  forSale = false;
-  canRedeem = true;
- }
+    function initialize(address _collection,address nft_owner, uint256 _tokenId, uint256 _amount) external onlyOwner {
+    require(_amount > 0, "Amount needs to be more than 0");
 
+    IERC721 collection = IERC721(_collection);
+    require(!fractionalizedNFTs[_tokenId].initialized, "NFT already initialized");
 
- function  redeem(uint256  _amount) external {
-  require(canRedeem, "Redemption not available");
-  uint256  totalETH = address(this).balance;
-  uint256  toRedeem = _amount * totalETH / totalSupply();
-  _burn(msg.sender, _amount);
-  payable(msg.sender).transfer(toRedeem);
- }
+    collection.safeTransferFrom(nft_owner, address(this), _tokenId);
+
+    fractionalizedNFTs[_tokenId] = FractionalizedNFTInfo({
+        collection: collection,
+        tokenId: _tokenId,
+        initialized: true,
+        forSale: false,
+        salePrice: 0,
+        canRedeem: false
+    });
+
+    _mint(nft_owner, _amount);
+}
+
+    function putForSale(uint256 _nftId, uint256 _price) external onlyOwner {
+        FractionalizedNFTInfo storage nftInfo = fractionalizedNFTs[_nftId];
+        require(nftInfo.initialized, "NFT not initialized");
+        nftInfo.salePrice = _price;
+        nftInfo.forSale = true;
+    }
+
+    function purchase(uint256 _nftId) external payable {
+        FractionalizedNFTInfo storage nftInfo = fractionalizedNFTs[_nftId];
+        require(nftInfo.forSale, "NFT not for sale");
+        require(msg.value >= nftInfo.salePrice, "Not enough funds sent");
+
+        nftInfo.collection.transferFrom(address(this), msg.sender, nftInfo.tokenId);
+        nftInfo.forSale = false;
+        nftInfo.canRedeem = true;
+    }
+
+    function redeem(uint256 _nftId, uint256 _amount) external {
+        FractionalizedNFTInfo storage nftInfo = fractionalizedNFTs[_nftId];
+        require(nftInfo.canRedeem, "Redemption not available");
+
+        uint256 totalCelo = address(this).balance;
+        uint256 toRedeem = (_amount * totalCelo) / totalSupply();
+        _burn(msg.sender, _amount);
+        payable(msg.sender).transfer(toRedeem);
+    }
 }
